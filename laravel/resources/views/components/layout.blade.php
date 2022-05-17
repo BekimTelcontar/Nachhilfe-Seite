bd50cb221683d782915d23e4cfd26e14ed032ab3
Coco-Rush <inazel.az.ri@gmail.com>
2022-05-17 23:06:32 +0200

    Fertig ig

--- laravel/resources/views/components/layout.blade.php
+++ laravel/resources/views/components/layout.blade.php
@@ -19,7 +19,6 @@
     <nav>
         <a href="/"><img src="{{ asset('Bilder/Rairlogo.png') }}" alt="Rairlogo" class="Rairlogo"></a>
         <div class="block">
-
             <div class="dropdown">
                 <a href="/tutoring" class="dropbtn">Nachhilfe geben</a>
             </div>
@@ -71,6 +70,40 @@
                     @endauth
                 </div>
             </div>
+        </div>
+        <Button class="hamburger-menu">
+            <div class="overlay">
+                <div class="sidebar-menu">
+                    <h3>Menu</h3>
+
+                    <a href="/tutors/1">Mathematik</a>
+                    <a href="/tutors/2">Deutsch</a>
+                    <a href="/tutors/3">Englisch</a>
+                    <a href="/tutors/4">Französisch</a>
+                    <a href="/tutors/5">Geschichte</a>
+                    <a href="/tutors/6">Geografie</a>
+                    <a href="/tutors/7">Physik</a>
+                    <a href="/tutors/8">Chemie</a>
+                    <a href="/tutors/9">Biologie</a>
+                    <a href="/tutors/10">Wirtschaft und Recht</a>
+                    <a href="/tutors/11">Finanz und Rechnungswesen</a>
+                    <a href="/tutors/12">Informatik</a>
+                    <a href="/tutors/13">Musik</a>
+                    <a href="/tutors/14">Sozialwissenschaften</a>
+                    <a href="/tutors/15">Bildn. Gestalten</a>
+                    <a href="/tutors/16">Religion und Ethik</a>
+                    <a href="/tutoring" >Nachhilfe geben</a>
+                    <a href="/register">Registrieren</a>
+                    @guest
+                        <a href="/login">Anmelden</a>
+                    @endguest
+                    @auth
+                        <a href="/account">Profil anschauen</a>
+                        <a href="/fs">Abmelden</a>
+                    @endauth
+
+                </div>
+        </button>
         </div>
     </nav><br>
 
